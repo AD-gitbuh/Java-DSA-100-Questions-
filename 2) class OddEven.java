bc46fad8c7c9d51771eefa1a8cdeff22d33d3707
Abhilash Dhyani <abhilash201204@gmail.com>
2025-08-13 23:36:48 +0530

    Added Q02: Check Even or Odd

---
+++ 2) class OddEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class OddEven
+{
+      public static void main(String []args)
+      {
+            Scanner rv=new Scanner(System.in);
+            System.out.println("Enter the number= ");
+            int a=rv.next();
+            if
+             {
+              a%2==0;
+              System.out.println("The number is even");
+             }
+            else 
+            {
+                System.out.println("The number is odd");
+            }
+       }
+}
